940039a056abef478d03b3582b45d9bcd405e411
vyklimavicius <vyklimavicius@gmail.com>
2019-12-09 16:30:07 -0500

    DB user change

--- db/migrate/20191114213434_create_users.rb
+++ db/migrate/20191114213434_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :lastname
       t.string :password_digest
       t.string :email
-      t.string :avatar
 
       t.timestamps
     end
